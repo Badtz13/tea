ae990715bb97405dfb53d711096bf6413178b057
Badtz <badtzmaru139@gmail.com>
2020-09-12 21:49:39 -0700

    update login route and ignore firebase cache files

--- src/router.js
+++ src/router.js
@@ -11,12 +11,8 @@ const router = new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '*',
-      redirect: '/login',
-    },
-    {
       path: '/',
-      redirect: '/login',
+      redirect: '/home',
     },
     {
       path: '/login',
