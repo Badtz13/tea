04e797faf071f7ba9c91ade0d6752089fdcbf5ec
Badtz <badtzmaru139@gmail.com>
2020-09-14 21:37:15 -0700

    begin signup page

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase/app';
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import Profile from './views/Profile.vue';
+
 
 Vue.use(Router);
 
@@ -11,44 +11,32 @@ const router = new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '*',
+      redirect: '/home',
+    },
+    {
       path: '/',
       redirect: '/home',
     },
     {
       path: '/login',
       name: 'Login',
-      component: () => import(/* webpackChunkName: "login" */ './views/Login.vue'),
+      component: () => import(/* webpackChunkName: "about" */ './views/Login.vue'),
     },
     {
       path: '/signup',
       name: 'Signup',
-      component: () => import(/* webpackChunkName: "signup" */ './views/Signup.vue'),
+      component: () => import(/* webpackChunkName: "about" */ './views/Signup.vue'),
     },
     {
       path: '/reset',
       name: 'Reset',
-      component: () => import(/* webpackChunkName: "reset" */ './views/Reset.vue'),
+      component: () => import(/* webpackChunkName: "about" */ './views/Reset.vue'),
     },
     {
       path: '/home',
-      name: 'home',
+      name: 'Home',
       component: Home,
-      meta: {
-        requiresAuth: false,
-      },
-    },
-    {
-      path: '/profile/:uid/',
-      name: 'profile',
-      component: Profile,
-    },
-    {
-      path: '/admin/review',
-      name: 'review',
-      component: () => import(/* webpackChunkName: "review" */ './views/admin/Review.vue'),
-      meta: {
-        requiresAuth: true,
-      },
     },
   ],
 });
@@ -59,7 +47,6 @@ router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
 
   if (requiresAuth && !currentUser) next('login');
-  else if (!requiresAuth && currentUser) next('home');
   else next();
 });
 
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,5 +1,46 @@
 <template>
   <div>
     <h1>Signup</h1>
+    <input class="border p-2 m-2" type="email" v-model="email">
+    <input class="border p-2 m-2" type="text" v-model="username">
+    <input class="border p-2 m-2" type="password" v-model="password">
+    <button @click="signup">Signup</button>
   </div>
 </template>
+
+<script>
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+export default ({
+  name: 'login',
+  components: {},
+  data() {
+    return {
+      email: '',
+      password: '',
+      username: '',
+    };
+  },
+  methods: {
+    signup() {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then((user) => {
+        firebase.database().ref('users').push().set({
+          uid: user.user.uid,
+          username: this.username,
+          joinDate: Date.now(),
+        })
+          .then(() => {
+            this.$router.replace('home');
+          });
+      },
+      (err) => {
+        // eslint-disable-next-line no-alert
+        alert(`Oops. ${err.message}`);
+      });
+    },
+  },
+});
+
+</script>
